d247935cdb81706e56dc06f322c4a6a8f8604188
Kaiden Richardson <kaidenr@shaw.ca>
2024-06-03 12:07:35 -0700

    Ported Cookie Crusade's Shadow Casting Sprite

--- Assets/Scripts/TestPackage.cs
+++ Assets/Scripts/TestPackage.cs
@@ -7,8 +7,8 @@ public class TestPackage : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        ZC.HelloRuntime hrt = GetComponent<ZC.HelloRuntime>();
-        hrt.ExternalCall();
-        Debug.Log(ZC.HelloNamespaced.NonComponentAction());
+        // ZC.HelloRuntime hrt = GetComponent<ZC.HelloRuntime>();
+        // hrt.ExternalCall();
+        // Debug.Log(ZC.HelloNamespaced.NonComponentAction());
     }
 }
